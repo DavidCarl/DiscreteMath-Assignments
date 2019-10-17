658478ef2a882a390b997b332a1915b8e8420b3f
davidcarl <david.martin.carl@gmail.com>
2019-10-17 16:30:14 +0200

    Starting out Assignment 6

---
+++ Assignment-6/variable.py
@@ -0,0 +1,18 @@
+class INTEGER():
+    name = ''
+    value = None
+
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+    
+
+
+class BOOLEAN():
+    name = ''
+    value = None
+
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
