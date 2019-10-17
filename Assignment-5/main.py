99452a29bdce1578df4d239aba05b1e22840ef5b
davidcarl <david.martin.carl@gmail.com>
2019-10-17 14:20:19 +0200

    Complement added

--- Assignment-5/main.py
+++ Assignment-5/main.py
@@ -8,7 +8,7 @@ def main():
     print(handler.intersection(set([1,2,3]), set([2,3,4])))
     print(handler.union(set([1,2,3]), set([2,3,4])))
     print(handler.difference(set([1,2,3]), set([2,3,4])))
-    print(handler.complement(set([1,2,3]), set([2,3,4])))
+    print(handler.complement(set([1,2,3,5]), set([2,3,4])))
     print(handler.subset(set([1,2,3]), set([2,3])))
 
 
